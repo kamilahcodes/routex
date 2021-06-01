4cc9c9ab05a060b99ed309bc2cd7a6d4cbe61fa4
Kamilah Agbaje <kamilahagbaje@gmail.com>
2021-06-01 17:39:35 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    BookingFormComponent
   ],
   imports: [
     BrowserModule,
